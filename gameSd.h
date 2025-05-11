cedbce8b017fc2fe7d58a92901270299b4082f2e
Sunakshi-ui <sunakshi1310@gmail.com>
2025-05-11 12:26:41 +0530

    Update gameSd.h

--- gameSd.h
+++ gameSd.h
@@ -2,11 +2,7 @@
 #define SUDOKU_H
 
 #include <stdbool.h>
-
 #define SIZE 9
-
-
-
 
 //linked list for generation
 typedef struct Cell {
@@ -23,28 +19,16 @@ bool generateGrid(int grid[SIZE][SIZE], Cell* emptyCells);
 //step 2:
 void removeNumbers(int grid[SIZE][SIZE], int numToRemove, Cell** emptyCells);
 
-
 //for solving
 // step 1: boolean masking
 bool rowMask[SIZE][SIZE + 1];  // rowMask[row][num]: true if 'num' is in the row
 bool colMask[SIZE][SIZE + 1];  // colMask[col][num]: true if 'num' is in the column
 bool gridMask[SIZE][SIZE + 1]; // gridMask[grid][num]: true if 'num' is in the subgrid
-
-
 //step 2:
 bool solveSudoku(int grid[SIZE][SIZE], int row, int col);
 
-
-
-
-
-
-
-
-
+//common functions
 bool isValid(int grid[SIZE][SIZE], int row, int col, int num);
-
-
 int getGridIndex(int row, int col); 
 void printGrid(int grid[SIZE][SIZE]);
 
